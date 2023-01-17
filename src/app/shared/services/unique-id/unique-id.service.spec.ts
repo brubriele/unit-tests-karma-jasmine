45afcf321bb7d997fbf8d31753cfc2f8b2e12fa9
brubriele <brunagabriele.paula@gmail.com>
2023-01-17 01:08:00 -0300

    :heavy_check_mark: Concludes module II with improved unite tests and test regex for id format, closes #2

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -3,9 +3,43 @@ import { UniqueServiceId } from "./unique-id.service";
 describe(UniqueServiceId.name, () => {
     // Tudo que diz respeito ao artefato que queremos testar
 
-    it(`#${UniqueServiceId.prototype.generateUniqueIdWithPrefix.name} should generate ID only when called with prefix`, () => {
-       const service = new UniqueServiceId;
-       const id = service.generateUniqueIdWithPrefix('app');
-       expect(id).toContain('app-')
+    let service: UniqueServiceId = null;
+    beforeEach(() => {
+        service = new UniqueServiceId();
     });
+
+    it(`#${UniqueServiceId.prototype.generateUniqueIdWithPrefix.name} 
+    should generate ID only when called with prefix`, () => {
+        const id = service.generateUniqueIdWithPrefix('app');
+        expect(id.startsWith('app-')).toBeTrue();
+    });
+
+    it(`#${UniqueServiceId.prototype.generateUniqueIdWithPrefix.name} 
+    should generate duplicated IDs when called multiple times`, () => {
+        const ids = new Set()
+        for (let i = 0; i < 50; i++) {
+            ids.add(service.generateUniqueIdWithPrefix('app-'))
+        }
+        expect(ids.size).toBe(50)
+    });
+
+    it(`#${UniqueServiceId.prototype.getNumberOfGenerateUniqueIds.name} 
+    should return the number of generateIDs when called`, () => {
+        service.generateUniqueIdWithPrefix('app')
+        service.generateUniqueIdWithPrefix('app')
+
+        expect(service.getNumberOfGenerateUniqueIds()).toBe(2)
+    });
+
+    it(`#${UniqueServiceId.prototype.generateUniqueIdWithPrefix.name} 
+    should throw error when called with empty arguments`, () => {
+        const emptyValues = [null, undefined, '', '0', '1']
+
+        emptyValues.forEach(emptyValue => {
+            expect(() => service.generateUniqueIdWithPrefix(emptyValue))
+            .withContext(`Empty value: ${emptyValue}`)
+            .toThrow();
+        });
+    });
+
 })
--- src/app/shared/services/unique-id/unique-id.service.ts
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -3,10 +3,12 @@ import { v4 as uuidv4 } from 'uuid'
 
 @Injectable()
 export class UniqueServiceId {
-    private numberOfGenerationIds = 0
+    private numberOfGenerationIds = 0;
+
+    private validId = /^[A-Za-z]+[\w\-\:\.]*$/;
 
     public generateUniqueIdWithPrefix(prefix: string): string {
-        if (!prefix) {
+        if (!prefix || !this.validId.test(prefix)) {
             throw Error('Prefix cannot be empty')
         }
         const uniqueId = this.generateUniqueId();
